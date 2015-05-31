29124ab979de4148d599dd45d8c30ef29073f82b
Felix <myflytop@gmail.com>
2015-05-31 14:06:43 +0800

    Add MusicSetting.h

--- RemioBeta/ViewController.m
+++ RemioBeta/ViewController.m
@@ -10,45 +10,7 @@
 #import "JZDragView.h"
 #import "AudioEngine.h"
 
-#define kMusicFile1 @"m1.mp3"
-#define kMusicSinger1 @"Armin van Buuren"
-#define kMusicTitle1 @"Ping Pong"
-#define kMusicImage1 @"ap1.png"
-
-#define kMusicFile2 @"m2.mp3"
-#define kMusicSinger2 @"GusGus"
-#define kMusicTitle2 @"CrossFade"
-#define kMusicImage2 @"ap2.png"
-
-#define kMusicFile3 @"m3.mp3"
-#define kMusicSinger3 @"Ferry Corsten"
-#define kMusicTitle3 @"Black Light"
-#define kMusicImage3 @"ap3.png"
-
-#define kMusicFile4 @"m4.mp3"
-#define kMusicSinger4 @"Flosstradamus"
-#define kMusicTitle4 @"Too Turnt Up"
-#define kMusicImage4 @"ap4.png"
-
-#define kMusicFile5 @"m5.mp3"
-#define kMusicSinger5 @"Clavin Harris"
-#define kMusicTitle5 @"Blame"
-#define kMusicImage5 @"ap5.png"
-
-#define kMusicFile6 @"m6.mp3"
-#define kMusicSinger6 @"Daft Punk"
-#define kMusicTitle6 @"Da Funk"
-#define kMusicImage6 @"ap6.png"
-
-#define kMusicFile7 @"m7.mp3"
-#define kMusicSinger7 @"Zedd"
-#define kMusicTitle7 @"Into the Lair"
-#define kMusicImage7 @"ap7.png"
-
-#define kMusicFile8 @"m8.mp3"
-#define kMusicSinger8 @"Avicii"
-#define kMusicTitle8 @"Wake Me Up"
-#define kMusicImage8 @"ap8.png"
+#import "MusicSetting.h"
 
 static NSString *const kBTSPulseAnimation = @"BTSPulseAnimation";
 
